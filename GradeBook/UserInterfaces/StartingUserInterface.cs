1962245c041830d580a583213768e7eca1878ba5
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2023-03-20 20:45:32 +0100

    type argument added to 'create' command to support ranked grading

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,29 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
-            var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
+
+            (string name, string type) = (parts[1], parts[2]);
+
+            BaseGradeBook gradeBook = type.ToLower() switch
+            {
+                "standard" => new StandardGradeBook(name),
+                "ranked" => new RankedGradeBook(name),
+                _ => null
+            };
+            
+            if (gradeBook == null)
+            {
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
+                return;
+            }
+
             GradeBookUserInterface.CommandLoop(gradeBook);
+            Console.WriteLine("Created gradebook {0}.", name);
         }
 
         public static void LoadCommand(string command)
