236c1d3af6b554c00930788a3d5dcfe8a9037011
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2023-03-20 22:37:05 +0100

    GPA bonus for selected student types

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,20 +109,27 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
-            switch (letterGrade)
+            int gradeScore = letterGrade switch
             {
-                case 'A':
-                    return 4;
-                case 'B':
-                    return 3;
-                case 'C':
-                    return 2;
-                case 'D':
-                    return 1;
-                case 'F':
-                    return 0;
+                'A' => 4,
+                'B' => 3,
+                'C' => 2,
+                'D' => 1,
+                _ => 0
+            };
+
+            if (IsWeighted)
+            {
+                int typeScore = studentType switch
+                {
+                    StudentType.Honors or StudentType.DualEnrolled => 1,
+                    _ => 0
+                };
+
+                return gradeScore + typeScore;
             }
-            return 0;
+
+            return gradeScore;
         }
 
         public virtual void CalculateStatistics()
